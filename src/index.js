32dbc58e2c044e1d3138d1c992a3d7e3b279f16b
김민결 <mickey.minkyuol@gmail.com>
2022-05-23 16:09:19 +0900

    INIT

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+
+import Jjres from "./jjres"
+
+const classes = [];
+
+document.getElementById("form").addEventListener("click", () => {
+    const jjrss = new Jjres("연희중", "abcd", "0", "0", "0", "홍길동", "");
+    jjrss.codeCheck();
+});
+
+document.getElementById("add").addEventListener("click", () => {
+    const className = prompt("과목명을 입력해 주세요.");
+
+    const classElement = document.createElement("li");
+    classElement.className = "list-group-item d-flex justify-content-between align-items-center";
+    classElement.innerText = className;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.type = "button";
+    deleteButton.className = "badge btn btn-danger btn-sm";
+    deleteButton.innerText = "삭제";
+    deleteButton.addEventListener("click", () => {
+        const index = classes.indexOf(className);
+        classes.splice(index, 1);
+        classElement.remove();
+    });
+
+    classElement.appendChild(deleteButton);
+
+    document.getElementById("classList").appendChild(classElement);
+
+    classes.push(className);
+});
---
+++ src/jjres.js
@@ -0,0 +1,44 @@
+const URL1 = "http://u.jjrss.com/user_001_json.php";
+const URL2 = "http://u.jjrss.com/user_201J.php";
+const URL3 = "http://u.jjrss.com/user_201V.php";
+
+export default class Jjres {
+    constructor(school, room, hak, bun, ban, name, classes) {
+        this.stdName = null;
+        this.diffDate = null;
+        this.userListTypeYn = null;
+        this.etcApplyCount = null;
+        this.etcType = null;
+        this.etcIdx = null;
+        this.schDiv = null;
+        this.tchCode = null;
+        this.schoolCode = null;
+        this.school = school;
+        this.room = room;
+        this.hak = hak;
+        this.bun = bun;
+        this.ban = ban;
+        this.name = name;
+        this.classes = classes;
+    }
+
+    main() {
+
+    }
+
+    codeCheck() {
+        const data = {
+            "key": encodeURIComponent(this.school),
+            "caseBy": "school_list"
+        };
+
+        console.log(data);
+
+        fetch(URL1, {
+            method: "POST",
+            body: JSON.stringify(data)
+        })
+            .then(r => r.text())
+            .then(console.log);
+    }
+}
